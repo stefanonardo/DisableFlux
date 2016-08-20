7a1e795ea35127eb613d7f79191d25c908f6a625
Stefano <Stefano@DESKTOP-AUHV2OU>
2016-08-21 00:20:50 +0200

    Add project files.

---
+++ DisableFlux/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace FluxProcess
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.buttonFileDialog = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(155, 82);
+            this.listBox1.TabIndex = 0;
+            // 
+            // buttonFileDialog
+            // 
+            this.buttonFileDialog.Location = new System.Drawing.Point(173, 12);
+            this.buttonFileDialog.Name = "buttonFileDialog";
+            this.buttonFileDialog.Size = new System.Drawing.Size(87, 26);
+            this.buttonFileDialog.TabIndex = 1;
+            this.buttonFileDialog.Text = "Choose .exe";
+            this.buttonFileDialog.UseVisualStyleBackColor = true;
+            this.buttonFileDialog.Click += new System.EventHandler(this.buttonFileDialog_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(173, 42);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(87, 26);
+            this.buttonDelete.TabIndex = 2;
+            this.buttonDelete.Text = "Delete from list";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "DisableFlux";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(174, 76);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(95, 17);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "Run at Startup";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(267, 101);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonFileDialog);
+            this.Controls.Add(this.listBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button buttonFileDialog;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ DisableFlux/Form1.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Management;
+using System.Timers;
+using Microsoft.Win32;
+
+namespace FluxProcess
+{
+    public partial class Form1 : Form
+    {
+        int runningCount = 0;
+        System.Timers.Timer timer = new System.Timers.Timer(3601000);
+
+        public Form1(string[] args)
+        {
+            InitializeComponent();
+
+            if (args.Length > 0 && "--start-minimized".Equals(args[0]))
+            {
+                WindowState = FormWindowState.Minimized;
+            }
+        }
+
+        /* It loads user settings. */
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
+
+            if (Properties.Settings.Default.RunAtStartup)
+            {
+                checkBox1.Checked = true;
+            }
+
+            foreach (string processName in Properties.Settings.Default.ProgramsList)
+            {
+                listBox1.Items.Add(processName);
+            }
+
+            for (int i = listBox1.Items.Count - 1; i >= 0; --i)
+            {
+                OnChangedList(listBox1.Items[i].ToString(), false);
+            }
+
+            WaitForProcess();
+        }
+
+        private void buttonFileDialog_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog1 = new OpenFileDialog())
+            {
+                openFileDialog1.InitialDirectory = @"C:\";
+                openFileDialog1.Filter = "Applications (*.exe)|*.exe";
+
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    string fileName = openFileDialog1.SafeFileName;
+
+                    if (listBox1.Items.IndexOf(fileName) >= 0)
+                    {
+                        return;
+                    }
+                    listBox1.Items.Add(fileName);
+                    Properties.Settings.Default.ProgramsList.Add(fileName);
+                    Properties.Settings.Default.Save();
+
+                    OnChangedList(fileName, false);
+                }
+            }
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            string curItem = listBox1.SelectedItem.ToString();
+            Properties.Settings.Default.ProgramsList.Remove(curItem);
+            Properties.Settings.Default.Save();
+            listBox1.Items.Remove(listBox1.SelectedItem);
+
+            OnChangedList(curItem, true);
+        }
+
+        /* It starts process watchers. */
+        private void WaitForProcess()
+        {
+            ManagementEventWatcher startWatch = new ManagementEventWatcher(
+                new WqlEventQuery("SELECT * FROM Win32_ProcessStartTrace"));
+            startWatch.EventArrived
+                += new EventArrivedEventHandler(startWatch_EventArrived);
+            startWatch.Start();
+
+            ManagementEventWatcher stopWatch = new ManagementEventWatcher(
+                new WqlEventQuery("SELECT * FROM Win32_ProcessStopTrace"));
+            stopWatch.EventArrived
+                += new EventArrivedEventHandler(stopWatch_EventArrived);
+            stopWatch.Start();
+        }
+
+        private void startWatch_EventArrived(object sender, EventArrivedEventArgs e)
+        {
+            string processName = e.NewEvent.Properties["ProcessName"].Value.ToString();
+
+            if (listBox1.Items.IndexOf(processName) >= 0)
+            {
+                if (runningCount == 0)
+                {
+                    SendKeys.SendWait("%({END})");
+                    timer.Start();
+                }
+                ++runningCount;
+            }
+
+        }
+
+        private void stopWatch_EventArrived(object sender, EventArrivedEventArgs e)
+        {
+            string processName = e.NewEvent.Properties["ProcessName"].Value.ToString();
+            if (processName.IndexOf(".exe") == -1)
+            {
+                processName += ".exe";
+            }
+
+            if (listBox1.Items.IndexOf(processName) >= 0)
+            {
+                if (runningCount == 1)
+                {
+                    SendKeys.SendWait("%({END})");
+                    timer.Stop();
+                }
+                --runningCount;
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (runningCount > 0)
+            {
+                SendKeys.SendWait("%({END})");
+            }
+        }
+
+        private void OnTimedEvent(object source, ElapsedEventArgs e)
+        {
+            SendKeys.SendWait("%({END})");
+        }
+
+        /* It checks the state of the app when listBox changes. */
+        private void OnChangedList(string fileName, bool deleting)
+        {
+            int isRunning = Process.GetProcessesByName(fileName.Substring(0, fileName.Length - 4)).Length;
+            switch (deleting)
+            {
+                case false:
+                    if (isRunning > 0 && runningCount == 0)
+                    {
+                        SendKeys.SendWait("%({END})");
+                        ++runningCount;
+                        timer.Start();
+                    }
+                    else if (isRunning > 0 && runningCount > 0)
+                    {
+                        ++runningCount;
+                    }
+                    break;
+                case true:
+                    if (isRunning > 0 && runningCount == 1)
+                    {
+                        SendKeys.SendWait("%({END})");
+                        --runningCount;
+                        timer.Stop();
+                    }
+                    else if (isRunning > 0 && runningCount > 1)
+                    {
+                        --runningCount;
+                    }
+                    break;
+            }
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            notifyIcon1.BalloonTipTitle = "Minimize to Tray App";
+            notifyIcon1.BalloonTipText = "The app has been minimized.";
+
+            if (WindowState == FormWindowState.Minimized)
+            {
+                notifyIcon1.Visible = true;
+                Hide();
+                if (Properties.Settings.Default.FirstStart)
+                {
+                    notifyIcon1.ShowBalloonTip(500);
+                    Properties.Settings.Default.FirstStart = false;
+                    Properties.Settings.Default.Save();
+                }
+            }
+            else if (WindowState == FormWindowState.Normal)
+            {
+                notifyIcon1.Visible = false;
+            }
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Show();
+            WindowState = FormWindowState.Normal;
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            using (RegistryKey rkApp = Registry.LocalMachine.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true))
+            {
+                if (checkBox1.Checked)
+                {
+                    Properties.Settings.Default.RunAtStartup = true;
+                    Properties.Settings.Default.Save();
+                    rkApp.SetValue("DisableFlux", Application.ExecutablePath.ToString() + " --start-minimized");
+                }
+                else
+                {
+                    Properties.Settings.Default.RunAtStartup = false;
+                    Properties.Settings.Default.Save();
+                    rkApp.DeleteValue("DisableFlux", false);
+                }
+            }
+        }
+    }
+}
